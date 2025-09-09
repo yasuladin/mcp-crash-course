2d173c76a3e374d6a2d8759d22b059b6d673ad5d
yasuladin <yasuhito.sakamoto@gmail.com>
2025-09-09 18:35:11 +0700

    main.pyにMCPクライアントとLangChainの依存関係を追加し、isortをpyproject.tomlに追加しました。また、isortのバージョン6.0.1をuv.lockに追加しました。

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 import asyncio
 from dotenv import load_dotenv
 import os
+
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
+
+from langchain_openai import ChatOpenAI
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langgraph.prebuilt import create_react_agent
+
 load_dotenv()
-print(os.getenv("OPENAI_API_KEY"))
+
+llm = ChatOpenAI(model="gpt-4o-mini")
+
+stdio_server_params = StdioServerParameters(
+    command="python",
+    args=["/Users/yasuhito/_dev/__Udemy/EdenMarco/mcp-crash-course/servers/math_server.py"],
+)
+
 
 async def main():
     print("Hello from mcp-crash-course!")
