ce662257ece838d0079f6cdd5138de1583a139f1
yasuladin <yasuhito.sakamoto@gmail.com>
2025-09-10 17:56:43 +0700

    main.pyにMCPクライアントの初期化とLangChainを使用したエージェントの実行機能を追加しました。ユーザーからのメッセージに対する応答を取得する処理を実装しました。

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import asyncio
 from dotenv import load_dotenv
 import os
 
+from langchain_core.messages import HumanMessage
 from mcp import ClientSession, StdioServerParameters
 from mcp.client.stdio import stdio_client
 
@@ -20,8 +21,17 @@ stdio_server_params = StdioServerParameters(
 
 
 async def main():
-    print("Hello from mcp-crash-course!")
+    async with stdio_client(stdio_server_params) as (read, write):
+        async with ClientSession(read_stream=read, write_stream=write) as session:
+            await session.initialize()
+            print("session initialized")
+            # tools = await session.list_tools()
+            tools = await load_mcp_tools(session)
 
+            agent = create_react_agent(llm, tools)
+
+            result = await agent.ainvoke({"messages": [HumanMessage(content="What is 2 + 2?")]})
+            print(result["messages"][-1].content)
 
 if __name__ == "__main__":
     asyncio.run(main())
